e473294a82ddfc24fa3dff404a8347d76891bb6c
Costa Giannakopoulos <costag1982@gmail.com>
2022-12-09 22:28:42 +0000

    working

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myBucket = new s3.Bucket(this, "cdk-test", {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,        
+      websiteIndexDocument: "index.html"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const deployment = new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
+      sources: [s3Deployment.Source.asset("../website")],
+      destinationBucket: myBucket
+    });
   }
 }
