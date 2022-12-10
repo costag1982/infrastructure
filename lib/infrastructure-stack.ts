c3ff8eb3795c54f0163f0454701fcadd7a315c2a
Costa Giannakopoulos <costag1982@gmail.com>
2022-12-10 23:13:00 +0000

    wip

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,20 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
-import { Construct } from 'constructs';
+import { App, Stack, StackProps } from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
 
-export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class InfrastructureStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props); 
 
-    const myBucket = new s3.Bucket(this, "cdk-test", {
-      publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,        
-      websiteIndexDocument: "index.html"
+    const myBucket = new s3.Bucket(this, "createBucket", {
+      bucketName: "static-site-cdk",
+      publicReadAccess: true,        
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html"
     });
 
-    const deployment = new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
-      sources: [s3Deployment.Source.asset("../website")],
+    new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
+      sources: [s3Deployment.Source.asset("./website")],
       destinationBucket: myBucket
     });
   }
